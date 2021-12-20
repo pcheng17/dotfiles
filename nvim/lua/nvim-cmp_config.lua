78b7678b1a42cf7544fe8b8da32977573bf063d5
Peter Cheng <pcheng17@gmail.com>
2021-12-20 11:04:18 -0800

    Update nvim-cmp settings

--- nvim/lua/nvim-cmp_config.lua
+++ nvim/lua/nvim-cmp_config.lua
@@ -1,12 +1,18 @@
 local cmp = require('cmp')
 
 cmp.setup {
+    completion = {
+        completeopt = 'menu,menuone,noinsert'
+    },
     snippet = {
         expand = function(args)
             vim.fn["UltiSnips#Anon"](args.body)
         end,
     },
     mapping = {
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-e>'] = cmp.mapping.close(),
         ['<CR>'] = cmp.mapping.confirm {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
@@ -15,11 +21,12 @@ cmp.setup {
     sources = {
         { name = 'nvim_lsp' },
         { name = 'ultisnips' },
+        { name = 'buffer' },
     },
 }
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- local servers = { 'clangd' }
 
