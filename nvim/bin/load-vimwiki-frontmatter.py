43755711e55d2dd5bad751d382b8100a27a6a86b
Peter Cheng <pcheng17@gmail.com>
2022-02-07 22:35:43 -0800

    Change template for journal entries

--- nvim/bin/load-vimwiki-frontmatter.py
+++ nvim/bin/load-vimwiki-frontmatter.py
@@ -17,12 +17,16 @@ template = [
     ""
 ]
 
-date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+now = datetime.datetime.now()
+date = now.strftime('%Y-%m-%d')
+timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
+dateFullStr = now.strftime('%B %d, %Y')
 dow = calendar.day_name[datetime.date.today().weekday()]
+
 if len(sys.argv) < 2:
     title = "New note"
     h1_title = title;
 else:
     title = os.path.basename(os.path.normpath(sys.argv[1].rsplit('.', 1)[0])).replace('_', ' ')
-    h1_title = f'{dow}, {date}' if title == date else title
-print('\n'.join(template).format(title=title, date=date, h1_title=h1_title))
+    h1_title = f'{dow} - {dateFullStr}' if title == date else title
+print('\n'.join(template).format(title=title, date=timestamp, h1_title=h1_title))
