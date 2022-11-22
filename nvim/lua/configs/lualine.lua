696a30dd1fa740e7d2d8b29f69456dcb9504a2bc
Peter Cheng <pcheng17@gmail.com>
2022-11-22 12:52:46 -0800

    Update lualine config

--- nvim/lua/configs/lualine.lua
+++ nvim/lua/configs/lualine.lua
@@ -1,14 +1,15 @@
 require('lualine').setup({
-    -- options = {
-    --     refresh = {
-    --         statusline = 10,
-    --     },
-    -- },
+    options = {
+        always_divide_middle = true,
+        icons_enabled = false,
+        component_separators = { left = '|', right = '|' },
+        section_separators = '',
+    },
     sections = {
         lualine_a = {'mode'},
         lualine_b = {'branch', 'diff', 'diagnostics'},
         lualine_c = {'filename'},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_x = {'filetype', 'encoding', 'fileformat'},
         lualine_y = {},
         lualine_z = {'location'}
     },
