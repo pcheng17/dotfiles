a1d2c60ae2bf32ed692d5e95a2b1fc7264cdd326
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2023-11-27 17:03:38 -0800

    Update ibl config

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,13 +1,16 @@
 return {
   {
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     event = { "BufReadPost", "BufNewFile" },
     opts = {
-      -- char = "▏",
-      char = "│",
-      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
-      show_trailing_blankline_indent = false,
-      show_current_context = true,
+      exclude = {
+        filetypes = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+      },
+      scope = {
+        enabled = true,
+      },
+      indent = {},
     },
   },
 }
