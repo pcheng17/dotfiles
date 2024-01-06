68141308cdee8d7194a8dd21fefe1e9190312f48
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-01-06 11:39:28 -0800

    Add autocmds

--- nvim/lua/pcheng.lua
+++ nvim/lua/pcheng.lua
@@ -26,3 +26,16 @@ require("lazy").setup("plugins")
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
+
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+local myGroup = augroup("pcheng", {})
+
+autocmd("TermOpen", {
+    group = myGroup,
+    pattern = "*",
+    command = "startinsert",
+})
+
