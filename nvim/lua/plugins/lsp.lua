0dd96b96e9cb964e3610dee5aad98824ff3ba7cf
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-01-06 11:26:02 -0800

    WIP: Following Prime, adding lsp

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,22 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+        require("mason").setup({})
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "clangd",
+                "cmake",
+                "lua_ls",
+            },
+            handlers = {
+                function(server_name)
+                    require("lspconfig")[server_name].setup({})
+                end,
+            },
+        })
+    end
+}
