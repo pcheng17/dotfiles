2b912c75349499eefd86f69d2d629e19703fbaec
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-01-09 00:37:40 -0800

    Updates

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -25,7 +25,9 @@ return {
         require("mason").setup({})
 
         -- TODO Figure out what this capabilities thing is...
-        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
         local lspconfig = require("lspconfig")
 
         require("mason-lspconfig").setup({
