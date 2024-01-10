2ca2048315787f61325b00497575ac0572a7a5f4
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-01-10 00:11:44 -0800

    Update gitsigns

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -12,25 +12,27 @@ return {
             },
             current_line_blame = true,
             current_line_blame_opts = {
-                delay = 500,
+                delay = 400,
             },
             on_attach = function(bufnr)
                 local gs = package.loaded.gitsigns
 
-                local function map(mode, lhs, rhs, opts)
-                    opts = opts or {}
-                    opts.buffer = bufnr
-                    vim.keymap.set(mode, lhs, rhs, opts)
+                local function keymap(mode, lhs, rhs, opts)
+                    local options = { buffer = bufnr, silent = true }
+                    if opts then
+                        options = vim.tbl_extend("force", options, opts)
+                    end
+                    vim.keymap.set(mode, lhs, rhs, options)
                 end
 
                 -- Navigation
-                map("n", "]h", function()
+                keymap("n", "]h", function()
                     if vim.wo.diff then return "]h" end
                     vim.schedule(function() gs.next_hunk() end)
                     return "<Ignore>"
                 end, { expr = true, desc = "Next hunk" })
 
-                map("n", "[h", function()
+                keymap("n", "[h", function()
                     if vim.wo.diff then return "[h" end
                     vim.schedule(function() gs.prev_hunk() end)
                     return "<Ignore>"
