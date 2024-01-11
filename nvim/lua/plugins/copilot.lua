33edeb43f5df584abdb59332f64879335dcea1c7
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-01-11 13:56:34 -0800

    Random updates

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,3 +1,15 @@
 return {
-    "github/copilot.vim"
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+        require("copilot").setup({
+            suggestion = {
+                auto_trigger = true,
+                keymap = {
+                    accept = "<tab>",
+                },
+            },
+        })
+    end,
 }
