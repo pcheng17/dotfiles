9144b4d8ee78efec0f781b7767449ab75f701128
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-01-15 23:07:14 -0800

    feat: add pyright and some settings

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -35,6 +35,7 @@ return {
                 "clangd",
                 "cmake",
                 "lua_ls",
+                "pyright",
             },
             handlers = {
                 function(server_name) -- default handler
@@ -49,7 +50,7 @@ return {
                         settings = {
                             Lua = {
                                 diagnostics = {
-                                    globals = { "vim" }
+                                    globals = { "exepath", "vim" }
                                 }
                             }
                         },
@@ -72,6 +73,37 @@ return {
                         end,
                     })
                 end,
+
+                ["pyright"] = function()
+                    require("lspconfig").pyright.setup({
+                        capabilities = capabilities,
+                        before_init = function(_, config)
+                            local function get_python_path(workspace)
+                                local util = require('lspconfig/util')
+                                local path = util.path
+
+                                -- Use activated virtualenv.
+                                if vim.env.VIRTUAL_ENV then
+                                    return path.join(vim.env.VIRTUAL_ENV, 'bin', 'python')
+                                end
+
+                                -- Find and use virtualenv in workspace directory.
+                                for _, pattern in ipairs({'*', '.*'}) do
+                                    local match = vim.fn.glob(path.join(workspace, pattern, 'pyvenv.cfg'))
+                                    if match ~= '' then
+                                        return path.join(path.dirname(match), 'bin', 'python')
+                                    end
+                                end
+
+                                -- Fallback to system Python.
+                                return exepath('python3') or exepath('python') or 'python'
+                            end
+
+                            config.settings.python.pythonPath = get_python_path(config.root_dir)
+                            print("Using Python interpreter: " .. config.settings.python.pythonPath)
+                        end,
+                    })
+                end
             }
         })
 
