6e7b1935704706a08abf455ec589b3ae30fdf98d
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-01-17 23:25:55 -0800

    Add more plugins but all disabled

---
+++ nvim/lua/plugins/vim-illuminate.lua
@@ -0,0 +1,14 @@
+return {
+    "RRethy/vim-illuminate",
+    enabled = false,
+    event = "VeryLazy",
+    config = function()
+        require("illuminate").configure({
+            delay = 200,
+            large_file_cutoff = 2000,
+            large_file_overrides = {
+                providers = { "lsp" }
+            },
+        })
+    end
+}
