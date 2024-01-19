126503a35826d6bbc3986252f33ca52f2f509757
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-01-18 23:55:43 -0800

    Modify overseer

--- nvim/lua/plugins/overseer.lua
+++ nvim/lua/plugins/overseer.lua
@@ -1,7 +1,18 @@
 return {
     "stevearc/overseer.nvim",
     config = function()
-        require("overseer").setup({})
+        require("overseer").setup({
+            task_list = {
+                bindings = {
+                    -- Disable defaults
+                    ["<C-l>"] = false,
+                    ["<C-h>"] = false,
+                    -- Add my own
+                    ["<Tab>"] = "IncreaseDetail",
+                    ["<S-Tab>"] = "DecreaseDetail",
+                }
+            }
+        })
 
         vim.keymap.set("n", "<leader>or", "<cmd>OverseerRun<cr>", { silent = true, desc = "Overseer Run" })
         vim.keymap.set("n", "<leader>ot", "<cmd>OverseerToggle!left<cr>", { silent = true, desc = "Overseer Toggle bottom" })
