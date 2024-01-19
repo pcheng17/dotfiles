e592c5a041f0a0f0eac3655f9a070aa24c946031
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-01-18 23:38:04 -0800

    Don't lazy load lazygit

--- nvim/lua/plugins/lazygit.lua
+++ nvim/lua/plugins/lazygit.lua
@@ -3,9 +3,6 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim", -- optional for floating window border deocoration
     },
-    keys = {
-        "<leader>lg"
-    },
     config = function()
         vim.keymap.set("n", "<leader>lg", "<cmd>LazyGit<cr>", { noremap = true, desc = "LazyGit" })
     end
