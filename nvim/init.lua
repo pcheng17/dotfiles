245ef8b898472757876316a3bc559c03cd1dc487
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-01-18 22:10:37 -0800

    Refactoring

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,33 @@
-require("pcheng")
+-- Boostrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    -- bootstrap lazy.nvim
+    -- stylua: ignore
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+require("lazy").setup("plugins", {
+    change_detection = {
+        enable = true,  -- Automatically check for config file changes and reload
+        notify = false, -- Disable notifications whenever plugin changes are made
+    }
+})
+
+require("core.options")
+require("core.keymaps")
+require("core.autocmds")
+
+-- Conditionally require work-related stuff
+-- pcall(require, "roblox")
+
