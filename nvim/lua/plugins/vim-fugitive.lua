e682ce6f0134a55c41f43f317cb9f6cf4a60bd47
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-01-20 22:44:40 -0800

    Fully remove keymap that I commented out

--- nvim/lua/plugins/vim-fugitive.lua
+++ nvim/lua/plugins/vim-fugitive.lua
@@ -3,7 +3,6 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>gs",  ":G status<cr>",       { silent = true, desc = "Git status" })
         vim.keymap.set("n", "<leader>gc",  ":G commit -v -q<cr>", { silent = true, desc = "Git commit" })
-        -- vim.keymap.set("n", "<leader>gps", ":Git push<cr>",       { silent = true, desc = "Git push" })
         vim.keymap.set("n", "<leader>gvd", ":Gvdiffsplit<cr>",    { silent = true, desc = "Git vertical diff split" })
         vim.keymap.set("n", "<leader>gw",  ":Gwrite<cr>",         { silent = true, desc = "Git write" })
         vim.keymap.set("n", "<leader>gr",  ":Gread<cr>",          { silent = true, desc = "Git read" })
