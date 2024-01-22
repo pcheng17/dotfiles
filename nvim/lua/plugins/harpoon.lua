a27b5a02a82774f5767120437463bc3ac60cd911
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-01-21 22:50:27 -0800

    feat: lazy load harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,29 +1,18 @@
 return {
     "ThePrimeagen/harpoon",
-    event = "VeryLazy",
+    keys = {
+        "<leader>,",
+        "<leader>hp",
+        "<M-h>",
+        "<M-j>",
+        "<M-k>",
+        "<M-l>",
+    },
     branch = "harpoon2",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local harpoon = require("harpoon")
         harpoon:setup({})
-
-        -- Basic telescope configuration
-        -- local conf = require("telescope.config").values
-        -- local function toggle_telescope(harpoon_files)
-        --     local file_paths = {}
-        --     for _, item in ipairs(harpoon_files.items) do
-        --         table.insert(file_paths, item.value)
-        --     end
-
-        --     require("telescope.pickers").new({}, {
-        --         prompt_title = "Harpoon",
-        --         finder = require("telescope.finders").new_table({
-        --             results = file_paths,
-        --         }),
-        --         previewer = conf.file_previewer({}),
-        --         sorter = conf.generic_sorter({}),
-        --     }):find()
-        -- end
 
         local keymap = vim.keymap.set
         keymap("n", "<leader>,",  function() harpoon:list():append() end,                      { silent = true, desc = "Add to Harpoon" })
@@ -32,7 +21,5 @@ return {
         keymap("n", "<M-j>",      function() harpoon:list():select(2) end,                     { silent = true, desc = "Harpoon 2" })
         keymap("n", "<M-k>",      function() harpoon:list():select(3) end,                     { silent = true, desc = "Harpoon 3" })
         keymap("n", "<M-l>",      function() harpoon:list():select(4) end,                     { silent = true, desc = "Harpoon 4" })
-
-        -- map("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Open harpoon window" }
     end
 }
