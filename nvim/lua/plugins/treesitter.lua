94ac7efac29261ee19088e47c76d0831b84637f4
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-01-24 00:02:41 -0800

    feat: Trying different treesitter keymaps

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -39,11 +39,11 @@ return {
             incremental_selection = {
                 enable = true,
                 keymaps = {
-                    init_selection = "<C-space>",
-                    node_incremental = "<C-space>",
+                    init_selection = "<CR>",
+                    node_incremental = "<CR>",
                     scope_incremental = false,
-                    node_decremental = "<C-BS>",
-                },
+                    node_decremental = "<Del>",
+                }
             },
             textobjects = {
                 select = {
