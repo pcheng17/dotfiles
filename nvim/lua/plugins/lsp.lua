b33968b9641355faed19161411859fb08062c998
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-01-28 00:58:55 -0800

    Disable some cmp stuff

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -135,22 +135,22 @@ return {
         })
 
         -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline({ '/', '?' }, {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-                { name = 'buffer' }
-            }
-        })
+        -- cmp.setup.cmdline({ '/', '?' }, {
+        --     mapping = cmp.mapping.preset.cmdline(),
+        --     sources = {
+        --         { name = 'buffer' }
+        --     }
+        -- })
 
         -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline(':', {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = 'path' }
-            }, {
-                    { name = 'cmdline' }
-                })
-        })
+        -- cmp.setup.cmdline(':', {
+        --     mapping = cmp.mapping.preset.cmdline(),
+        --     sources = cmp.config.sources({
+        --         { name = 'path' }
+        --     }, {
+        --             { name = 'cmdline' }
+        --         })
+        -- })
 
         -- TODO Do I need this?
         -- vim.diagnostic.config({
