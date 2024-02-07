0601b07ede1c08aec417375dd3f2725ce97c55ec
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-02-07 15:44:47 -0800

    Disable treesitter context

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -119,6 +119,7 @@ return {
     },
     {
         "nvim-treesitter/nvim-treesitter-context",
+        enabled = false,
         config = function()
             vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true, sp = "Grey"})
             vim.api.nvim_set_hl(0, "TreesitterContextLineNumberBottom", { underline = true, sp = "Grey"})
