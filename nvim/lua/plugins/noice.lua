af63ea744c34bb52e3aef9841fc865b45386dc8c
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-02-09 23:55:47 -0800

    Enable noice for now

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,14 +1,9 @@
 return {
     "folke/noice.nvim",
-    enabled = false,
+    enabled = true,
     event = "VeryLazy",
     dependencies = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
         "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
-        -- "rcarriga/nvim-notify",
     },
     config = function()
         require("noice").setup({
@@ -20,17 +15,19 @@ return {
                     ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
                 },
             },
-            -- you can enable a preset for easier configuration
             presets = {
                 bottom_search = true, -- use a classic bottom cmdline for search
                 command_palette = true, -- position the cmdline and popupmenu together
                 long_message_to_split = true, -- long messages will be sent to a split
                 inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                lsp_doc_border = false, -- add a border to hover docs and signature help
+                lsp_doc_border = true, -- add a border to hover docs and signature help
             },
-            notify = {
-                enabled = false
-            },
+            -- notify = {
+            --     enabled = false
+            -- },
+            -- messages = {
+            --     enabled = false
+            -- }
         })
     end
 }
