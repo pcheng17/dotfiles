7293ceab8984fb5a5f2c0bd1f2516ea1fb92eeb2
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-02-13 18:08:01 -0800

    Add alpha

---
+++ nvim/lua/plugins/alpha.lua
@@ -0,0 +1,26 @@
+return {
+    'goolord/alpha-nvim',
+    enabled = false,
+    config = function ()
+        local dashboard = require("alpha.themes.dashboard")
+
+        dashboard.section.header.val = {
+            "                                                     ",
+            "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+            "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+            "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+            "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+            "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+            "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+            "                                                     ",
+        }
+
+        -- Set menu
+        dashboard.section.buttons.val = {
+            dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
+            dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+        }
+
+        require('alpha').setup(dashboard.opts)
+    end
+};
