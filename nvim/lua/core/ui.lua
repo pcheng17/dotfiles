e02827747cf75316807e8cc8aa75d95327d51523
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-05-29 17:12:14 -0700

    Disable transparency

--- nvim/lua/core/ui.lua
+++ nvim/lua/core/ui.lua
@@ -1,10 +1,12 @@
-vim.api.nvim_set_hl(0, "Normal", { ctermbg = "none" })
-vim.api.nvim_set_hl(0, "NormalNC", { ctermbg = "none" })
-vim.api.nvim_set_hl(0, "NonText", { ctermbg = "none" })
-vim.api.nvim_set_hl(0, "SignColumn", { ctermbg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { ctermbg = "none" })
-vim.api.nvim_set_hl(0, "StatusLine", { ctermbg = "none" })
-vim.api.nvim_set_hl(0, "StatusLineNC", { ctermbg = "none" })
+if false then
+    vim.api.nvim_set_hl(0, "Normal", { ctermbg = "none" })
+    vim.api.nvim_set_hl(0, "NormalNC", { ctermbg = "none" })
+    vim.api.nvim_set_hl(0, "NonText", { ctermbg = "none" })
+    vim.api.nvim_set_hl(0, "SignColumn", { ctermbg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { ctermbg = "none" })
+    vim.api.nvim_set_hl(0, "StatusLine", { ctermbg = "none" })
+    vim.api.nvim_set_hl(0, "StatusLineNC", { ctermbg = "none" })
 
-vim.api.nvim_set_hl(0, "NeoTreeNormal", { ctermbg = "none" })
-vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { ctermbg = "none" })
+    vim.api.nvim_set_hl(0, "NeoTreeNormal", { ctermbg = "none" })
+    vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { ctermbg = "none" })
+end
