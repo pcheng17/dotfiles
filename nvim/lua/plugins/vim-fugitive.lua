fcd0c0a00b2dc7ae6bfa2cf7f172727a67772bb1
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-06-05 14:39:00 -0700

    Disable my own fugitive keymaps because I'm not using them

--- nvim/lua/plugins/vim-fugitive.lua
+++ nvim/lua/plugins/vim-fugitive.lua
@@ -1,12 +1,12 @@
 return {
     "tpope/vim-fugitive",
     config = function()
-        vim.keymap.set("n", "<leader>gs",  ":G status<cr>",       { silent = true, desc = "Git status" })
-        vim.keymap.set("n", "<leader>gc",  ":G commit -v -q<cr>", { silent = true, desc = "Git commit" })
-        vim.keymap.set("n", "<leader>gvd", ":Gvdiffsplit<cr>",    { silent = true, desc = "Git vertical diff split" })
-        vim.keymap.set("n", "<leader>gw",  ":Gwrite<cr>",         { silent = true, desc = "Git write" })
-        vim.keymap.set("n", "<leader>gr",  ":Gread<cr>",          { silent = true, desc = "Git read" })
-        vim.keymap.set("x", "do",          ":diffget<cr>",        { silent = true, desc = "Git diff get" })
-        vim.keymap.set("x", "dp",          ":diffput<cr>",        { silent = true, desc = "Git diff put" })
+        -- vim.keymap.set("n", "<leader>gs",  ":G status<cr>",       { silent = true, desc = "Git status" })
+        -- vim.keymap.set("n", "<leader>gc",  ":G commit -v -q<cr>", { silent = true, desc = "Git commit" })
+        -- vim.keymap.set("n", "<leader>gvd", ":Gvdiffsplit<cr>",    { silent = true, desc = "Git vertical diff split" })
+        -- vim.keymap.set("n", "<leader>gw",  ":Gwrite<cr>",         { silent = true, desc = "Git write" })
+        -- vim.keymap.set("n", "<leader>gr",  ":Gread<cr>",          { silent = true, desc = "Git read" })
+        -- vim.keymap.set("x", "do",          ":diffget<cr>",        { silent = true, desc = "Git diff get" })
+        -- vim.keymap.set("x", "dp",          ":diffput<cr>",        { silent = true, desc = "Git diff put" })
     end
 }
