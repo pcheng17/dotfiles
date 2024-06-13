3548ee4c88083593707ce8a31042273b0ecb1861
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-06-13 10:53:04 -0700

    Updates

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -8,11 +8,15 @@ return {
         require('oil').setup({
             default_file_explorer = true,
             delete_to_trash = true,
+            skip_confirm_for_simple_edits = true,
             view_options = {
                 show_hidden = true,
                 is_always_hidden = function(name)
-                    return name == '.DS_Store'
+                    return name == '.DS_Store' or name == ".git"
                 end
+            },
+            win_options = {
+                wrap = true,
             },
         })
 
