519669e0f50d4c9b8e16d4f6bcac39311ee97458
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-07-22 23:36:27 -0700

    Add extra config to colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,7 @@ return {
         priority = 1000,
         config = function()
             require('catppuccin').setup({
+                transparent_background = false,
                 flavour = "mocha",
                 no_italic = true,
                 integrations = {
