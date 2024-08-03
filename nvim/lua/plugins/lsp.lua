5dccc64aa00528f049894f7f88bad017a5ea15ea
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-08-03 13:24:00 -0400

    Remove dead code

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -139,59 +139,25 @@ return {
                 documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
-                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-                ['<Tab>'] = cmp.mapping(function(fallback)
+                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<C-e>"] = cmp.mapping.abort(),
+                ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                ["<Tab>"] = cmp.mapping(function(fallback)
                     if vim.snippet.active({ direction = 1 }) then
                         vim.snippet.jump(1)
                     else
                         fallback()
                     end
                 end, { 'i', 's' }),
-                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
                     if vim.snippet.active({ direction = -1 }) then
                         vim.snippet.jump(-1)
                     else
                         fallback()
                     end
                 end, { 'i', 's' }),
-                -- ["<Tab>"] = vim.schedule_wrap(function(fallback)
-                --     if luasnip.expand_or_jumpable() then
-                --         luasnip.expand_or_jump()
-                --     else
-                --         fallback()
-                --     end
-                -- end),
-                -- ["<S-Tab>"] = vim.schedule_wrap(function(fallback)
-                --     if luasnip.jumpable(-1) then
-                --         luasnip.jump(-1)
-                --     else
-                --         fallback()
-                --     end
-                -- end),
-                -- Deprecating old behavior, but keeping code around just in case
-                -- Take a look at copilot-cmp's github for an implementation of `if cmp.visible()`
-                -- ["<Tab>"] = cmp.mapping(function(fallback)
-                --     if cmp.visible() then
-                --         cmp.select_next_item()
-                --     elseif luasnip.expand_or_jumpable() then
-                --         luasnip.expand_or_jump()
-                --     else
-                --         fallback()
-                --     end
-                -- end, { "i", "s" }),
-                -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-                --     if cmp.visible() then
-                --         cmp.select_prev_item()
-                --     elseif luasnip.jumpable(-1) then
-                --         luasnip.jump(-1)
-                --     else
-                --         fallback()
-                --     end
-                -- end, { "i", "s" }),
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
