0a5c15c97041e52b4ec373fafe46bfcd7ce5adfe
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2024-10-23 21:11:53 -0700

    Update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -19,6 +19,7 @@ config.window_padding = {
 config.hide_tab_bar_if_only_one_tab = true
 config.show_tab_index_in_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
+config.adjust_window_size_when_changing_font_size = false
 
 -- Font configuration
 config.color_scheme = "Catppuccin Mocha"
