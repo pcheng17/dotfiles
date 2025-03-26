650698c1671507f964f4a8856281257d96a86fac
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2025-03-25 21:33:42 -0700

    Styling to Vague, disable zk-nvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -87,7 +87,33 @@ return {
         priority = 1000,
         config = function()
             require("vague").setup({
-                -- optional configuration here
+                style = {
+                    -- "none" is the same thing as default. But "italic" and "bold" are also valid options
+                    boolean = "none",
+                    number = "none",
+                    float = "none",
+                    error = "none",
+                    comments = "italic",
+                    conditionals = "none",
+                    functions = "none",
+                    headings = "bold",
+                    operators = "none",
+                    strings = "none",
+                    variables = "none",
+
+                    -- keywords
+                    keywords = "none",
+                    keyword_return = "none",
+                    keywords_loop = "none",
+                    keywords_label = "none",
+                    keywords_exception = "none",
+
+                    -- builtin
+                    builtin_constants = "none",
+                    builtin_functions = "none",
+                    builtin_types = "none",
+                    builtin_variables = "none",
+                },
             })
         end
     },
