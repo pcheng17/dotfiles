f9d880b5eb9f0c9d05ea3a1ef805504024e00f4c
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2025-04-20 11:32:38 -0700

    Updates

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -5,4 +5,5 @@ vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.textwidth = 80
+vim.opt_local.breakindent = true
 -- vim.opt_local.conceallevel = 0
