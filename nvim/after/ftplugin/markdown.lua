3adff22f0b427706415add7632174d046d7995ed
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2025-04-20 01:41:43 -0700

    Change markdown width to 80

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -4,5 +4,5 @@ vim.opt_local.linebreak = true
 vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
-vim.opt_local.textwidth = 100
+vim.opt_local.textwidth = 80
 -- vim.opt_local.conceallevel = 0
