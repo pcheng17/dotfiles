d99b29b50635d7b59444ca838a14c3d393941513
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2025-05-04 01:27:09 -0700

    Add new open pdf workflow

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -34,6 +34,8 @@ alias j='just'
 alias ts='tmux-sessionizer'
 alias love='/Applications/love.app/Contents/MacOS/love'
 
+alias pdf='./bin/open-pdf'
+
 alias houdaemon='sudo launchctl load -w /Library/LaunchDaemons/com.sidefx.sesinetd.plist'
 alias pandock='docker run --rm -v "$(pwd):/data" -u $(id -u):$(id -g) pandoc/extra'
 
