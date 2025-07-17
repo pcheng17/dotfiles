1f66bd3018b3ecb9f44934d37de0daf5c6156acd
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2025-07-17 00:39:45 -0700

    Update typst for neovim

--- nvim/after/ftplugin/typst.lua
+++ nvim/after/ftplugin/typst.lua
@@ -1,8 +1,9 @@
 vim.opt_local.spell = true
 vim.opt_local.spelllang = "en_us"
 vim.opt_local.linebreak = true
-vim.opt_local.shiftwidth = 4
-vim.opt_local.tabstop = 4
-vim.opt_local.softtabstop = 4
-vim.opt_local.textwidth = 100
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.textwidth = 80
 -- vim.opt_local.conceallevel = 0
+
