478d418123d50c138ae393a14667e28f66f569e2
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2025-08-12 19:11:34 -0700

    Add keybindings

--- nvim/lua/plugins/typst.lua
+++ nvim/lua/plugins/typst.lua
@@ -1,6 +1,11 @@
 return {
     'chomosuke/typst-preview.nvim',
     ft = 'typst',
+    keys = {
+        { '<leader>tp', '<cmd>TypstPreviewToggle<cr>', desc = 'Typst Preview Toggle' },
+        { '<leader>ts', '<cmd>TypstPreviewStop<cr>', desc = 'Typst Preview Stop' },
+        { '<leader>tc', '<cmd>TypstPreviewFollowCursor<cr>', desc = 'Typst Preview Follow Cursor' },
+    },
     version = '1.*',
     opts = {},  -- lazy.nvim will implicitly calls `setup {}`
 }
