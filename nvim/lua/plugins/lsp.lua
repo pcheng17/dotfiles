0158f9a001fd504505cb4c9b0502330887368da8
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2025-08-25 01:23:44 -0400

    Change lsp menu

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -138,8 +138,14 @@ return {
                 end,
             },
             window = {
-                completion = cmp.config.window.bordered(),
-                documentation = cmp.config.window.bordered(),
+                completion = cmp.config.window.bordered({
+                    border = 'rounded',
+                    winhighlight = 'NormalFloat:Normal,FloatBorder:Normal'
+                }),
+                documentation = cmp.config.window.bordered({
+                    border = 'rounded',
+                    winhighlight = 'NormalFloat:Normal,FloatBorder:Normal'
+                }),
             },
             mapping = cmp.mapping.preset.insert({
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
