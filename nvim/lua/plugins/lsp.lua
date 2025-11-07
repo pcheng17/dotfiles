e342d61486c1febfdba5c8a95a191366d21488b1
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2025-11-06 22:37:47 -0800

    Add `cmp-nvim-lua`

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -13,6 +13,7 @@ return {
         { "williamboman/mason.nvim", config = true },
         "williamboman/mason-lspconfig.nvim",
         "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-nvim-lua",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
