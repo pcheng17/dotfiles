97f5bdb3b181e467138f0295733b5476215b50c3
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2025-11-30 00:28:20 -0800

    Add flutter plugins

---
+++ nvim/lua/plugins/flutter.lua
@@ -0,0 +1,17 @@
+return {
+    'nvim-flutter/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = function()
+        require('flutter-tools').setup({
+            lsp = {
+                color = {
+                    enabled = true,
+                }
+            }
+        })
+    end,
+}
