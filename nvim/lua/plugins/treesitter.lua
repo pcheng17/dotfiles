35d2c01fe2f18791f1c17fff34834f8186d938a7
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2026-01-04 17:10:34 -0800

    Add `odin` to treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -29,6 +29,7 @@ return {
                     "luap",
                     "markdown",
                     "markdown_inline",
+                    "odin",
                     "python",
                     "query",
                     "regex",
