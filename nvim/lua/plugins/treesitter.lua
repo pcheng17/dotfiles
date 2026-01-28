427dc04dc051667709a75b2467bf213c4ff3b29f
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2026-01-28 08:45:07 -0800

    Update treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
         },
         build = ":TSUpdate",
         config = function()
-            require("nvim-treesitter.configs").setup({
+            require("nvim-treesitter").setup({
                 highlight = { enable = true },
                 indent = { enable = true },
                 rainbow = { enable = false }, -- this is supposed to fix javascript issues...?
