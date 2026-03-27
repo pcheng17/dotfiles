fc5a0c4dd1a78d7a0787fbd91b6e66e71c95d3d8
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2026-03-27 16:56:34 -0700

    Add new alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -33,6 +33,7 @@ alias j='just'
 alias ts='tmux-sessionizer'
 alias oc='opencode'
 alias c='claude'
+alias cy='claude --dangerously-skip-permissions'
 
 alias tsu='sudo tailscale up'
 alias tsd='sudo tailscale down'
