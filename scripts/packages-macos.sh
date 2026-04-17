c183cd48135d6201e74d2f767b6c87db19b7650b
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2026-04-17 05:29:16 +0000

    Extract log helpers into helper/log.sh

--- scripts/packages-macos.sh
+++ scripts/packages-macos.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 
-log_ok() {
-    printf "\033[32m[OK]\033[0m   %s\n" "$1"
-}
-
-log_info() {
-    printf "\033[34m[INFO]\033[0m %s\n" "$1"
-}
-
-log_warn() {
-    printf "\033[33m[WARN]\033[0m %s\n" "$1"
-}
-
-log_error() {
-    printf "\033[31m[ERROR]\033[0m %s\n" "$1"
-}
+. "${DOTFILES_DIR}/helper/log.sh"
 
 BREW_TAPS=(
     "jesseduffield/lazygit"
