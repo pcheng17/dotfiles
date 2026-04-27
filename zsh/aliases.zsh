7d5085008a6f052b38b18024b7cebb5d0de72fa6
Peter Cheng <7921604+pcheng17@users.noreply.github.com>
2026-04-27 10:19:12 -0700

    Update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,11 +1,9 @@
-current_hostname=$(hostname)
-
 _exists() {
     command -v $1 > /dev/null 2>&1
 }
 
 alias l='ls -lhF'
-alias ll='ls -lhF'
+# alias ll='ls -lhF' # l and ll are identical!
 alias la='ls -AlhF'
 alias qq='~ && clear'
 alias rg='rg --smart-case'
@@ -25,7 +23,7 @@ alias gc='git commit'
 
 alias gwa='git worktree add'
 alias gwls='git worktree list'
-alias gupb='CBRANCH=$(git branch --show-current); git fetch origin $CBRANCH && g merge origin/$CBRANCH'
+alias gupb='b=$(git branch --show-current); git fetch origin $b && git merge origin/$b'
 alias gcob='git branch | fzf --height=50% | xargs git checkout'
 alias gbd='git branch | fzf -m | xargs git branch -D'
 alias uncommit='git reset --soft HEAD~1'
@@ -39,27 +37,34 @@ alias c='claude'
 alias tsu='sudo tailscale up'
 alias tsd='sudo tailscale down'
 
-alias pdf='./bin/open-pdf'
-
 alias houdaemon='sudo launchctl load -w /Library/LaunchDaemons/com.sidefx.sesinetd.plist'
-alias pandock='docker run --rm -v "$(pwd):/data" -u $(id -u):$(id -g) pandoc/extra'
 
-case "$hostname" in
+case "$OSTYPE" in
     darwin*)
         alias love='/Applications/love.app/Contents/MacOS/love'
     ;;
-    home)
-    ;;
 esac
 
-# Change this to use `case`
-if [[ "$current_hostname" == "HQ-VP606YTPQ2" ]]; then
-    # Work machine
-    alias wk='cd ~/work'
-    alias bkupbrew='TMP="/private/tmp/brewfile_backup_$(date +%s)" && brew bundle dump --file="$TMP" --force && grep -v "^vscode" "$TMP" > ~/.dotfiles/private/Brewfile && rm "$TMP"'
-else
-    # Personal machines
-    alias dv='cd ${DEV_DIR}'
-    alias bkupbrew='TMP="/private/tmp/brewfile_backup_$(date +%s)" && brew bundle dump --file="$TMP" --force && grep -v "^vscode" "$TMP" > ~/.dotfiles/Brewfile && rm "$TMP"'
-fi
+case "$HOST" in
+    HQ-VP606YTPQ2)
+        alias bkupbrew='TMP="/private/tmp/brewfile_backup_$(date +%s)" && brew bundle dump --file="$TMP" --force && grep -v "^vscode" "$TMP" > ~/.dotfiles/private/Brewfile && rm "$TMP"'
+        ;;
+    *)
+        alias bkupbrew='TMP="/private/tmp/brewfile_backup_$(date +%s)" && brew bundle dump --file="$TMP" --force && grep -v "^vscode" "$TMP" > ~/.dotfiles/Brewfile && rm "$TMP"'
+        ;;
+esac
 
+gwrm() {
+    if ! _exists fzf; then
+        git worktree remove "$@"
+        return
+    fi
+    local wt
+    wt=$(git worktree list | fzf --prompt="Remove worktree> " | awk '{print $1}')
+    [[ -z "$wt" ]] && return 0
+    if ! git worktree remove "$wt" 2>/dev/null; then
+        read -rq "REPLY?Worktree has changes. Force remove? [y/N] "
+        echo
+        [[ "$REPLY" =~ ^[Yy]$ ]] && git worktree remove --force "$wt"
+    fi
+}
